d20e438cdca29c162c00f268dcc439618c006f1b
shadab-malik <img_2020057@iiitm.ac.in>
2024-02-28 21:01:12 +0530

    adjusted logo size

--- src/pages/public/SigninForm.tsx
+++ src/pages/public/SigninForm.tsx
@@ -63,69 +63,71 @@ const SignUp = () => {
   };
 
   return (
-    <Form {...form}>
-      <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.svg" alt="logo" />
+    <div className="p-10">
+      <Form {...form}>
+        <div className="sm:w-420 flex-center flex-col">
+          <img src="/assets/images/logo.svg" className="w-72" alt="logo" />
 
-        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-8">
-          Create a new account
-        </h2>
-        <p className="text-light-3 small-medium md:base-regular mt-2">
-          To use Snapgram, please enter your details
-        </p>
-
-        <form
-          onSubmit={form.handleSubmit(submitHandler)}
-          className="flex flex-col gap-5 w-full mt-4"
-        >
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input type="email" className="shad-input" {...field} />
-                </FormControl>
-                <FormMessage className="shad-form_message" />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input type="password" className="shad-input" {...field} />
-                </FormControl>
-                <FormMessage className="shad-form_message" />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className="shad-button_primary">
-            {signInIsPending ? (
-              <div className="flex-center gap-2">
-                <Loader />
-              </div>
-            ) : (
-              "Sign In"
-            )}
-          </Button>
-
-          <p className="text-small-regular text-light-2 text-center mt-2">
-            Don't have account?
-            <Link
-              to={"/sign-up"}
-              className="text-primary-500 text-small-semibold ml-1"
-            >
-              Sign up
-            </Link>
+          <h2 className="h3-bold md:h2-bold pt-5 sm:pt-8">
+            Create a new account
+          </h2>
+          <p className="text-light-3 small-medium md:base-regular mt-2">
+            To use Snapgram, please enter your details
           </p>
-        </form>
-      </div>
-    </Form>
+
+          <form
+            onSubmit={form.handleSubmit(submitHandler)}
+            className="flex flex-col gap-5 w-full mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" className="shad-input" {...field} />
+                  </FormControl>
+                  <FormMessage className="shad-form_message" />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" className="shad-input" {...field} />
+                  </FormControl>
+                  <FormMessage className="shad-form_message" />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="shad-button_primary">
+              {signInIsPending ? (
+                <div className="flex-center gap-2">
+                  <Loader />
+                </div>
+              ) : (
+                "Sign In"
+              )}
+            </Button>
+
+            <p className="text-small-regular text-light-2 text-center mt-2">
+              Don't have account?
+              <Link
+                to={"/sign-up"}
+                className="text-primary-500 text-small-semibold ml-1"
+              >
+                Sign up
+              </Link>
+            </p>
+          </form>
+        </div>
+      </Form>
+    </div>
   );
 };
 
